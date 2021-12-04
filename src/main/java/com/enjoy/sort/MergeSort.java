545a63074c5ce66b3d61cbd03ae5a0582adede3f
shihua1412 <15000819116@163.com>
2021-12-04 19:03:41 +0800

    modify MergeSort.java

--- src/main/java/com/enjoy/sort/MergeSort.java
+++ src/main/java/com/enjoy/sort/MergeSort.java
@@ -4,13 +4,19 @@ import java.util.Arrays;
 
 /**
  * @ClassName: MergeSort
- * @Description: TODO
+ * @Description: 归并排序
  * @author: 陈晓勇
  * @date: 2021/12/4  10:45
  */
 public class MergeSort {
 
     public static void main(String[] args) {
+        /**
+         * 分而治之(divide - conquer);每个递归过程涉及三个步骤
+         * 第一, 分解: 把待排序的 n 个元素的序列分解成两个子序列, 每个子序列包括 n/2 个元素.
+         * 第二, 治理: 对每个子序列分别调用归并排序MergeSort, 进行递归操作
+         * 第三, 合并: 合并两个排好序的子序列,生成排序结果.
+         */
         int[] arr = {2, 4, 322, 3, 44, 6, 87, 61};
         int[] sort = sort(arr);
         System.out.println(Arrays.toString(sort));
